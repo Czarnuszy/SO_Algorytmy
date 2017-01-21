0bbfadb7d337274f1e3f70ca7bca30757977affb
Czarnuszy <gedaj.bartek@gmail.com>
2017-01-21 23:19:36 +0100

    SJF + Priority sort

--- SO_algorytmy/Priority.py
+++ SO_algorytmy/Priority.py
@@ -8,12 +8,39 @@ def printData(queue):
     for i in range(len(queue)):
         print('\t', queue[i].index, '\t\t', queue[i].arrival, '\t\t', queue[i].burst, "\t\t\t", queue[i].priority)
 
+def printGrid(queue2):
+    for i in range(len(queue2)):
+        queue2[i].print()
+    print('S \t', end='')
+    m = len(queue2)
+    x = queue2[m - 1].e_time
+    y = queue2[0].arrival
+    for i in range(y, x):
+        if i < 10:
+            print('| ' + str(i) + " ", end='')
+        else:
+            print('| ' + str(i), end='')
+
+def aging(process_queue):
+    queue2 = []
+    for i in range(len(process_queue)):
+        if process_queue[i].w_time >= 10:
+            process_queue[i].priority = 1
+    queue2.append(process_queue[0])
+    for i in range(1, len(process_queue)):
+        temp = []
+        for j in range(1, len(process_queue)):
+            if process_queue[j].arrival <= (queue2[i-1].burst+queue2[i-1].arrival):
+                temp.append(process_queue[j])
+        temp.sort(key=lambda temp: temp.priority, reverse=True)
+        queue2.append(temp[i-1])
+    return queue2
+
 
 def calculate(process_queue):
     start_time = 0
     average_wait = 0
     m = len(process_queue)
-    print(process_queue[0].s_time)
     for i in range(0, m):
         if process_queue[i].arrival > start_time:
             start_time = process_queue[i].arrival
@@ -21,19 +48,10 @@ def calculate(process_queue):
         else:
             process_queue[i].w_time = start_time - process_queue[i].arrival
         average_wait += process_queue[i].w_time
-        process_queue[i].print()
+
         process_queue[i].e_time = start_time + process_queue[i].burst
         start_time += process_queue[i].burst
-
-
-    print('S \t', end='')
-    x = process_queue[m - 1].e_time
-    y = process_queue[0].arrival
-    for i in range(y, x):
-        if i < 10:
-            print('| ' + str(i) + " ", end='')
-        else:
-            print('| ' + str(i), end='')
+    printGrid(process_queue)
     print("\nTotal wait time= ", average_wait)
     print("Average process wait time: ", float(average_wait / m))
 
@@ -53,8 +71,13 @@ def start():
         for j in range(1, len(process_queue)):
             if process_queue[j].arrival <= (queue2[i-1].burst+queue2[i-1].arrival):
                 temp.append(process_queue[j])
-        temp.sort(key=lambda p_queue: p_queue.priority)
+        temp.sort(key=lambda temp: temp.priority)
         queue2.append(temp[i-1])
     print("\nData input after sort:")
+    printData(queue2)
+    print("Before process aging: ")
+    calculate(queue2)
+    print("\n\nAfter process aging: ")
+    aging(queue2)
     printData(queue2)
     calculate(queue2)
